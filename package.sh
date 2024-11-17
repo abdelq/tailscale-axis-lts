cf48e0b8380ae6a50b0bccd86692f656bafb90ef
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2024-11-16 19:48:51 -0500

    Updating version in package.conf

--- package.sh
+++ package.sh
@@ -4,6 +4,16 @@ ACAP_ARCH=armv7hf
 ACAP_VERSION=3.1
 TAILSCALE_VERSION=1.76.6
 
+MAJOR_VERSION=$(echo $TAILSCALE_VERSION | cut -d. -f1)
+MINOR_VERSION=$(echo $TAILSCALE_VERSION | cut -d. -f2)
+MICRO_VERSION=$(echo $TAILSCALE_VERSION | cut -d. -f3)
+
+sed -i '' \
+    -e "s/APPMAJORVERSION=\"[0-9]*\"/APPMAJORVERSION=\"${MAJOR_VERSION}\"/" \
+    -e "s/APPMINORVERSION=\"[0-9]*\"/APPMINORVERSION=\"${MINOR_VERSION}\"/" \
+    -e "s/APPMICROVERSION=\"[0-9]*\"/APPMICROVERSION=\"${MICRO_VERSION}\"/" \
+    app/package.conf
+
 curl --remote-name --output-dir app \
     https://raw.githubusercontent.com/tailscale/tailscale/refs/tags/v${TAILSCALE_VERSION}/LICENSE
 
