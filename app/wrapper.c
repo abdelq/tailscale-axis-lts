9a479d28263f81f49276ea98cefb5fa46ec1595b
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2024-11-17 22:34:17 -0500

    Using absolute paths and making sure ownership is right

--- app/wrapper.c
+++ app/wrapper.c
@@ -5,10 +5,11 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    int exit_code = EXIT_SUCCESS;
     openlog("tailscaled", LOG_PID | LOG_CONS, LOG_USER);
 
-    int exit_code = EXIT_SUCCESS;
-    if (execv("lib/tailscaled", argv) == -1) {
+    argv[0] = "/usr/local/packages/tailscaled/lib/tailscaled";
+    if (execv(argv[0], argv) == -1) {
         syslog(LOG_ERR, "Failed to execute tailscaled: %s", strerror(errno));
         exit_code = EXIT_FAILURE;
     }
